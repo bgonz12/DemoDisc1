d49d1880e8584fb1770b33afa6fb50d57905aa3c
bgonz12 <bgonz12@hotmail.com>
2020-03-23 22:29:24 -0600

    Set up platformer character and controller

---
+++ Source/DemoDisc1/PlatformerDemo/PlatformerCharacterController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlatformerCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMODISC1_API APlatformerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
