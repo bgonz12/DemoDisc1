16b41313c8112ad5c4686e804653b6377e8f8a40
bgonz12 <bgonz12@hotmail.com>
2020-03-26 03:15:27 -0600

    Blocked out the platformer demo level

--- Source/DemoDisc1/PlatformerDemo/PlatformerCharacter.cpp
+++ Source/DemoDisc1/PlatformerDemo/PlatformerCharacter.cpp
@@ -45,8 +45,8 @@ APlatformerCharacter::APlatformerCharacter()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-	JumpGravity = 1.5f;
-	FallGravity = 2.0f;
+	JumpGravity = 1.0f;
+	FallGravity = 2.5f;
 }
 
 // Called when the game starts or when spawned
