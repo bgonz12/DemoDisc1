c5acddbe911dcd36b9d21606e1932666f8a4a9df
bgonz12 <bgonz12@hotmail.com>
2020-03-26 23:38:39 -0600

    Add rolling boulder

---
+++ Source/DemoDisc1/PlatformerDemo/PlatformerChaser.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlatformerChaser.h"
+#include "Components/SplineComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+//#include "ActorSequenceComponent.h"
+
+#include "PlatformerCharacter.h"
+
+// Sets default values
+APlatformerChaser::APlatformerChaser()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+	SetRootComponent(SplineComponent);
+
+	StaticMeshContainer = CreateDefaultSubobject<USceneComponent>(TEXT("StaticMeshContainer"));
+	StaticMeshContainer->SetupAttachment(RootComponent);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(StaticMeshContainer);
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	TriggerBox->SetupAttachment(RootComponent);
+
+	//BoulderFallSequence = CreateDefaultSubobject<UActorSequenceComponent>(TEXT("BoulderFallSequence"));
+
+	MoveSpeed = 500.0f;
+}
+
+// Called when the game starts or when spawned
+void APlatformerChaser::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &APlatformerChaser::BeginOverlap);
+
+	EndLocation = SplineComponent->GetWorldLocationAtTime(SplineComponent->Duration);
+}
+
+// Called every frame
+void APlatformerChaser::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsChasing)
+	{
+		FVector TargetLocation = StaticMeshContainer->GetComponentLocation() + GetActorForwardVector() * MoveSpeed * DeltaTime;
+
+		FTransform FinalTransform = SplineComponent->FindTransformClosestToWorldLocation(TargetLocation, ESplineCoordinateSpace::World);
+		StaticMeshContainer->SetWorldTransform(FinalTransform);
+
+		StaticMesh->AddLocalRotation(FRotator(-MoveSpeed * DeltaTime / 2.0f, 0.0f, 0.0f));
+
+		if (FVector::Dist(StaticMeshContainer->GetComponentLocation(), EndLocation) <= MoveSpeed * DeltaTime * 2.0f)
+		{
+			StopChasing();
+		}
+	}
+}
+
+void APlatformerChaser::StartChasing()
+{
+	//BoulderFallSequence
+	bIsChasing = true;
+}
+
+void APlatformerChaser::StopChasing()
+{
+	bIsChasing = false;
+}
+
+void APlatformerChaser::BeginOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (!bIsChasing && Cast<APlatformerCharacter>(OtherActor))
+	{
+		StartChasing();
+	}
+}
+
---
+++ Source/DemoDisc1/PlatformerDemo/PlatformerChaser.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformerChaser.generated.h"
+
+UCLASS()
+class DEMODISC1_API APlatformerChaser : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformerChaser();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	bool bIsChasing;
+
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	class USceneComponent* StaticMeshContainer;
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	class USplineComponent* SplineComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* TriggerBox;
+
+	//UPROPERTY(VisibleAnywhere)
+	//class UActorSequenceComponent* BoulderFallSequence;
+
+	FVector EndLocation;
+
+	void StartChasing();
+
+	void StopChasing();
+
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent,	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+};
