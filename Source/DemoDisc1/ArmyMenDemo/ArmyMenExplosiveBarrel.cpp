9ee4c471bb6450b0a79f03ced69925d9dda810d2
bgonz12 <bgonz12@hotmail.com>
2020-04-10 00:11:48 -0600

    Add Explosive Barrels

---
+++ Source/DemoDisc1/ArmyMenDemo/ArmyMenExplosiveBarrel.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ArmyMenExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/DamageType.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Sets default values
+AArmyMenExplosiveBarrel::AArmyMenExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(StaticMesh);
+
+	MaxHealth = 2;
+	CurrentHealth = 0;
+
+	ExplosionDamage = 5;
+	ExplosionRadius = 400.0f;
+}
+
+// Called when the game starts or when spawned
+void AArmyMenExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	bIsDead = false;
+
+	CurrentHealth = MaxHealth;
+}
+
+void AArmyMenExplosiveBarrel::Reset()
+{
+	bIsDead = false;
+
+	CurrentHealth = MaxHealth;
+
+	StaticMesh->SetVisibility(true);
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void AArmyMenExplosiveBarrel::Kill()
+{
+	if (bIsDead) return;
+
+	bIsDead = true;
+
+	StaticMesh->SetVisibility(false);
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(this);
+
+	UGameplayStatics::ApplyRadialDamageWithFalloff(
+		this,
+		ExplosionDamage,
+		ExplosionDamage,
+		GetActorLocation(),
+		ExplosionRadius,
+		ExplosionRadius,
+		0.0f,
+		TSubclassOf<UDamageType>(UDamageType::StaticClass()),
+		IgnoreActors,
+		this);
+
+	if (true)
+	{
+		UWorld* World = GetWorld();
+		if (!World) return;
+
+		UKismetSystemLibrary::DrawDebugSphere
+		(
+			World,
+			GetActorLocation(),
+			ExplosionRadius,
+			16,
+			FLinearColor::Red,
+			5.0f,
+			1.0f
+		);
+	}
+}
+
+float AArmyMenExplosiveBarrel::TakeDamage(float DamageAmount, FDamageEvent const & DamageEvent, AController * EventInstigator, AActor * DamageCauser)
+{
+	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	int Damage = FMath::FloorToInt(DamageAmount);
+
+	CurrentHealth -= Damage;
+
+	if (CurrentHealth <= 0)
+	{
+		CurrentHealth = 0;
+
+		Kill();
+	}
+
+	return DamageAmount;
+}
+
---
+++ Source/DemoDisc1/ArmyMenDemo/ArmyMenExplosiveBarrel.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ArmyMenExplosiveBarrel.generated.h"
+
+UCLASS()
+class DEMODISC1_API AArmyMenExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AArmyMenExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Reset() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Health)
+	int MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Health)
+	int CurrentHealth;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Health)
+	bool bIsDead;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Explosion)
+	int ExplosionDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Explosion)
+	int ExplosionRadius;
+
+	void Kill();
+
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+};
